a55205f5cd4db16b5c4a35ee0a558059148f03d7
whdrms6098 <whdrms6098@naver.com>
2024-06-28 23:09:41 +0900

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,6 +97,8 @@ const groupGallery = gsap.timeline({
     //     gsap.to(".group-gallery .img-item img", { y: -50 });
     //   }
     // },
+
+    //이질감 스크롤?
   },
 });
 
@@ -136,7 +138,143 @@ const interior = gsap
       start: "0% 0%",
       end: "100% 100%",
       scrub: 0,
-      markers: true,
+      // markers: true,
+      invalidateOnRefresh: true,
+      // onUpdate: function (self) {
+      //   const direction = self.direction;
+      //   if (direction == 1) {
+      //     gsap.to(".sc-interior .group-interior .item-title", { x: -50 });
+      //   } else {
+      //     gsap.to(".sc-interior .group-interior .item-title", { x: +50 });
+      //   }
+      // },
+
+      //이질감 스크롤?
     },
   })
-  .to(".sc-interior .interior-list", { xPercent: -100 });
+  .to(".sc-interior .interior-list", {
+    xPercent: -100,
+    x: function () {
+      return window.innerWidth;
+    },
+  });
+
+$(`.sc-member .group-header [data-effect="y-move"]`).each(function (index) {
+  const member = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".sc-member",
+        start: "0% 90%",
+        end: "10% 60%",
+        scrub: 0,
+        // markers: true,
+      },
+    })
+    .to($(this), { yPercent: 0, delay: index * 0.1 });
+});
+
+const orderInterior1 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-order-interior .group-img",
+      start: "-20% 60%",
+      end: "100% 100%",
+      scrub: 0,
+      // markers: true,
+    },
+  })
+  .to(".sc-order-interior .group-img", 2, { yPercent: -50 });
+
+const orderInterior2 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-order-interior .group-slide",
+      start: "0% 0",
+      end: "100% 100%",
+      scrub: 0,
+      // markers: true,
+      invalidateOnRefresh: true,
+    },
+  })
+  .to(".sc-order-interior .group-slide .video-area", { scale: 3.5 }, "slide")
+  .to(
+    `.sc-order-interior .group-slide [data-effect="x-move"]`,
+    { xPercent: 0 },
+    "slide+=.25"
+  )
+  .to(".sc-order-interior .group-slide .content-wrap", {
+    xPercent: -100,
+    x: function () {
+      return window.innerWidth;
+    },
+  });
+
+const interiorImg1 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(2)",
+      start: "0% 80%",
+      end: "80% 90%",
+      scrub: 0,
+      // markers: true,
+    },
+  })
+  .to(".sc-order-interior .group-interior .img-box1", { height: 0 });
+
+const interiorImg2 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(3)",
+      start: "0% 80%",
+      end: "80% 90%",
+      scrub: 0,
+      // markers: true,
+    },
+  })
+  .to(".sc-order-interior .group-interior .img-box2", { height: 0 });
+
+const interiorText1 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(2)",
+      start: "0% 55%",
+      end: "50% 55%",
+      scrub: 0,
+      // markers: true,
+    },
+  })
+  .to(".sc-order-interior .group-interior .text-box1", { height: 0 }, "text")
+  .to(".sc-order-interior .group-interior .text-box2", { height: 150 }, "text");
+
+const interiorText2 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(3)",
+      start: "0% 55%",
+      end: "80% 90%",
+      scrub: 0,
+      // markers: true,
+    },
+  })
+  .to(".sc-order-interior .group-interior .text-box2", { height: 0 }, "text")
+  .to(".sc-order-interior .group-interior .text-box3", { height: 150 }, "text");
+
+const textBg = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".sc-visual",
+      start: "0% 50%",
+      end: "100% 100%",
+      endTrigger: "#footer",
+      scrub: 0,
+      // markers: true,
+    },
+  })
+  .to(`.sc-visual [data-effect="y-move"]`, { yPercent: 0 });
+
+$(".sc-visual, #footer").mousemove(function (e) {
+  const x = (20 * e.clientX) / $(".sc-visual").width();
+  const y = (10 * e.clientY) / $(".sc-visual").height();
+
+  $(".sc-visual .img-box").css("transform", `translate(${x}px, -${y}px)`);
+});
