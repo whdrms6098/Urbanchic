7fcdbeaadfe28646d047b0c25853c5425670f2c3
whdrms6098 <whdrms6098@naver.com>
2024-07-30 01:52:25 +0900

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,262 +1,267 @@
-gsap.set(`[data-effect="fade"]`, { opacity: 0 });
-gsap.set(`[data-effect="x-move"]`, { xPercent: 100 });
-gsap.set(`[data-effect="y-move"]`, { yPercent: 100 });
+const lenis = new Lenis();
 
-const artText = gsap.timeline({});
+lenis.on("scroll", ScrollTrigger.update);
 
-$(".group-art .title-area span").each(function () {
-  artText.to($(this), { y: 0, duration: 0.2 });
+gsap.ticker.add((time) => {
+  lenis.raf(time * 800);
 });
 
-const artSubText = gsap.timeline({
-  delay: 0.5,
+gsap.ticker.lagSmoothing(0);
+
+const visualText1 = new SplitType(".sc-intro .sub-title-area", {
+  types: "words, chars",
+});
+const visualText2 = new SplitType(".sc-intro .title-area", {
+  types: "words, chars",
 });
 
-$(".group-art .sub-title-area span").each(function () {
-  artSubText.to($(this), { y: 0, duration: 0.2 });
-});
+const introTl = gsap.timeline({});
+introTl.from(
+  ".group-art .sub-title-area .char",
+  { yPercent: 100, duration: 0.2, stagger: 0.1 },
+  "a"
+);
+introTl.from(
+  ".group-art .title-area .char",
+  { yPercent: 100, duration: 0.2, stagger: 0.1 },
+  "a"
+);
+introTl.to(".group-art .desc-area p", { opacity: 1, duration: 1 }, "a");
+introTl.fromTo(".group-slide .img-area img", { scale: 1.2 }, { scale: 1, duration: 1 }, "a");
 
-artSubText.to(".group-art .desc-area p", { opacity: 1, duration: 1 }, "-=1.25");
+let mm = gsap.matchMedia();
+mm.add("(min-width: 1000px)", () => {
+  gsap.set(`[data-effect="fade"]`, { opacity: 0 });
+  gsap.set(`[data-effect="x-move"]`, { xPercent: 100 });
+  gsap.set(`[data-effect="y-move"]`, { yPercent: 100 });
+  gsap.defaults({
+    ease: "none",
+  });
 
-artSubText.to(".group-slide .img-area img", { scale: 1, duration: 1 }, "-=1.5");
-
-const introBg = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-intro .group-art",
-      start: "0% 0%",
-      end: "100% 100%",
-      endTrigger: ".sc-intro .group-slide .img-area",
-      scrub: 0,
-      //   markers: true,
-    },
-  })
-  .to(".group-slide", { padding: 0 });
-
-const groupSlide = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-intro .group-slide",
-      start: "0% 0%",
-      end: "100% 100%",
-      scrub: 0,
-      //   markers: true,
-      invalidateOnRefresh: true,
-    },
-  })
-  .to(
-    ".sc-intro .content-wrap",
-    1,
-    {
-      xPercent: -100,
-      x: function () {
-        return (
-          $(".sc-intro .area1").outerWidth() -
-          $(".sc-intro .wander-img").outerWidth()
-        );
-        //wander-img 넓이 값만큼 빼야 되는 이유?
+  const introBg = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".sc-intro .group-art",
+        start: "0% 0%",
+        end: "100% 100%",
+        endTrigger: ".sc-intro .group-slide .img-area",
+        scrub: 0,
+        //   markers: true,
       },
+    })
+    .to(".group-slide", { padding: 0 });
+
+  const groupSlide = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".sc-intro .group-slide",
+        start: "0% 0%",
+        end: "100% 100%",
+        scrub: 0,
+        //   markers: true,
+        invalidateOnRefresh: true,
+      },
+    })
+    .to(
+      ".sc-intro .content-wrap",
+      {
+        x: function () {
+          return -$(".sc-intro .area1").outerWidth() + window.innerWidth;
+        },
+      },
+      "bg"
+    )
+    .fromTo(".sc-intro .img-area img", { scale: 1 }, { scale: 0.84 }, "bg")
+    .to(".sc-intro .text-area-bg", { width: "100%" })
+    .to(
+      ".sc-intro .content-wrap",
+      {
+        xPercent: -100,
+        x: function () {
+          return window.innerWidth;
+        },
+      },
+      "text"
+    )
+    .to(".sc-intro .area2 .text-list", { yPercent: -100 }, "list")
+    .to(
+      ".sc-intro .area2 .img-list",
+      {
+        yPercent: -100,
+        y: function () {
+          return window.innerHeight;
+        },
+      },
+      "list+=0.04"
+    )
+    .to(".sc-intro .area2 .img-item", { y: -200 }, "list+=0.06");
+
+  gsap.to(".wander-img figure", {
+    xPercent: -250,
+    scrollTrigger: {
+      trigger: ".sc-intro .group-slide .wander-img",
+      containerAnimation: groupSlide,
+      start: "left 100%",
+      end: "left 0%",
+      scrub: 0,
+      ease:"none",
+      markers:true,
     },
-    "bg"
-  )
-  .to(".sc-intro .wander-img", { xPercent: -100 }, "bg")
-  .to(".sc-intro .img-area img", { scale: 0.84 }, "bg")
-  .to(".sc-intro .text-area-bg", { width: "100%" })
-  .to(
-    ".sc-intro .content-wrap",
+  });
+
+  gsap.fromTo(
+    ".gallery-area .img-list",
     {
+      x: 300,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".sc-intro .group-slide .gallery-area",
+        containerAnimation: groupSlide,
+        start: "left 100%",
+        end: "left 80%",
+        scrub: 0,
+        // markers: true,
+      },
+      x: 0,
+    }
+  );
+
+  gsap.fromTo(
+    ".sc-order .bg",
+    {
+      yPercent: -20,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".sc-order",
+        start: "0% 100%",
+        end: "100% 0%",
+        scrub: 0,
+      },
+      yPercent: 20,
+    }
+  );
+
+  gsap.to('.sc-order .group-call [data-effect="y-move"]', {
+    scrollTrigger: {
+      trigger: ".sc-order .title-area",
+      start: "0% 80%",
+      end: "100% 80%",
+      scrub: 0,
+      // markers: true,
+    },
+    yPercent: 0,
+    stagger: 0.01,
+  });
+
+  gsap.to(`.sc-order .group-order [data-effect="y-move"]`, {
+    scrollTrigger: {
+      trigger: ".sc-order",
+      start: "50% 80%",
+      end: "100% 100%",
+      scrub: 0,
+      // markers: true,
+    },
+    yPercent: 0,
+    stagger: 0.01,
+  });
+
+  const interior = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".group-interior",
+        start: "0% 0%",
+        end: "100% 100%",
+        scrub: 0,
+        // markers: true,
+        invalidateOnRefresh: true,
+      },
+    })
+    .to(".sc-interior .interior-list", {
       xPercent: -100,
       x: function () {
         return window.innerWidth;
       },
-    },
-    "text"
-  )
-  .to(`.sc-intro .area2 [data-effect="x-move"]`, { xPercent: 0 }, "text");
+    });
 
-const groupGallery = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".sc-intro .group-gallery",
-    start: "0% 0%",
-    end: "100% 100%",
-    scrub: 0,
-    // markers: true,
-    onEnter: function () {
-      gsap.set(`[data-effect="fade"]`, { opacity: 1 });
-      $(".sc-intro .area2 .gallery-area").addClass("remove");
+  gsap.to('.sc-member .group-header [data-effect="y-move"]', {
+    scrollTrigger: {
+      trigger: ".sc-member",
+      start: "0% 90%",
+      end: "10% 60%",
+      scrub: 0,
+      // markers: true,
     },
-    onLeaveBack: function () {
-      gsap.set(`[data-effect="fade"]`, { opacity: 0 });
-      $(".sc-intro .area2 .gallery-area").removeClass("remove");
-    },
-    // onUpdate: function (self) {
-    //   const direction = self.direction;
-    //   if (direction == 1) {
-    //     gsap.to(".group-gallery .img-item img", { y: +50 });
-    //   } else {
-    //     gsap.to(".group-gallery .img-item img", { y: -50 });
-    //   }
-    // },
+    yPercent: 0,
+    y: 0,
+    stagger: 0.01,
+  });
 
-    //이질감 스크롤?
-  },
-});
-
-$(`.sc-order .group-call [data-effect="y-move"]`).each(function (index) {
-  const call = gsap
+  const orderInterior1 = gsap
     .timeline({
       scrollTrigger: {
-        trigger: ".sc-order",
-        start: "0% 80%",
-        end: "50% 100%",
-        scrub: 0,
-        // markers: true,
-      },
-    })
-    .to($(this), { yPercent: 0, delay: index * 0.1 });
-});
-
-$(`.sc-order .group-order [data-effect="y-move"]`).each(function (index) {
-  const order = gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: ".sc-order",
-        start: "50% 80%",
+        trigger: ".sc-order-interior .group-img",
+        start: "-20% 60%",
         end: "100% 100%",
         scrub: 0,
         // markers: true,
       },
     })
-    .to($(this), { yPercent: 0, delay: index * 0.1 });
-});
+    .to(".sc-order-interior .group-img", 2, { yPercent: -50 });
 
-const interior = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".group-interior",
-      start: "0% 0%",
-      end: "100% 100%",
-      scrub: 0,
-      // markers: true,
-      invalidateOnRefresh: true,
-      // onUpdate: function (self) {
-      //   const direction = self.direction;
-      //   if (direction == 1) {
-      //     gsap.to(".sc-interior .group-interior .item-title", { x: -50 });
-      //   } else {
-      //     gsap.to(".sc-interior .group-interior .item-title", { x: +50 });
-      //   }
-      // },
-
-      //이질감 스크롤?
-    },
-  })
-  .to(".sc-interior .interior-list", {
-    xPercent: -100,
-    x: function () {
-      return window.innerWidth;
-    },
-  });
-
-$(`.sc-member .group-header [data-effect="y-move"]`).each(function (index) {
-  const member = gsap
+  const orderInterior2 = gsap
     .timeline({
       scrollTrigger: {
-        trigger: ".sc-member",
-        start: "0% 90%",
-        end: "10% 60%",
+        trigger: ".sc-order-interior .group-slide",
+        start: "0% 0",
+        end: "100% 100%",
         scrub: 0,
         // markers: true,
+        invalidateOnRefresh: true,
       },
     })
-    .to($(this), { yPercent: 0, delay: index * 0.1 });
+    .to(".sc-order-interior .group-slide .video-area", { scale: 3.5 }, "slide")
+    .to(
+      `.sc-order-interior .group-slide [data-effect="x-move"]`,
+      { xPercent: 0 },
+      "slide+=.25"
+    )
+    .to(".sc-order-interior .group-slide .content-wrap", {
+      xPercent: -100,
+      x: function () {
+        return window.innerWidth;
+      },
+    });
 });
 
-const orderInterior1 = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-order-interior .group-img",
-      start: "-20% 60%",
-      end: "100% 100%",
-      scrub: 0,
-      // markers: true,
-    },
-  })
-  .to(".sc-order-interior .group-img", 2, { yPercent: -50 });
+mm.add("(max-width:1000px)", () => {
+  gsap.set(`[data-effect="fade"]`, { opacity: 1 });
+  gsap.set(`[data-effect="x-move"]`, { xPercent: 0 });
+  gsap.set(`[data-effect="y-move"]`, { yPercent: 0, y: 0 });
+})
 
-const orderInterior2 = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-order-interior .group-slide",
-      start: "0% 0",
-      end: "100% 100%",
-      scrub: 0,
-      // markers: true,
-      invalidateOnRefresh: true,
-    },
-  })
-  .to(".sc-order-interior .group-slide .video-area", { scale: 3.5 }, "slide")
-  .to(
-    `.sc-order-interior .group-slide [data-effect="x-move"]`,
-    { xPercent: 0 },
-    "slide+=.25"
-  )
-  .to(".sc-order-interior .group-slide .content-wrap", {
-    xPercent: -100,
-    x: function () {
-      return window.innerWidth;
-    },
-  });
-
-const interiorImg1 = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(2)",
-      start: "0% 80%",
-      end: "80% 90%",
-      scrub: 0,
-      // markers: true,
-    },
-  })
-  .to(".sc-order-interior .group-interior .img-box1", { height: 0 });
-
-const interiorImg2 = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(3)",
-      start: "0% 80%",
-      end: "80% 90%",
-      scrub: 0,
-      // markers: true,
-    },
-  })
-  .to(".sc-order-interior .group-interior .img-box2", { height: 0 });
-
-const interiorText1 = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(2)",
-      start: "0% 55%",
-      end: "50% 55%",
-      scrub: 0,
-      // markers: true,
-    },
-  })
-  .to(".sc-order-interior .group-interior .text-box1", { height: 0 }, "text")
-  .to(".sc-order-interior .group-interior .text-box2", { height: 150 }, "text");
-
-const interiorText2 = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".sc-order-interior .group-interior .bg-area img:nth-child(3)",
-      start: "0% 55%",
-      end: "80% 90%",
-      scrub: 0,
-      // markers: true,
-    },
-  })
-  .to(".sc-order-interior .group-interior .text-box2", { height: 0 }, "text")
-  .to(".sc-order-interior .group-interior .text-box3", { height: 150 }, "text");
+interiorList = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.bg-area',
+    start: "0% 0%",
+    end: "100% 100%",
+    scrub: 0,
+    // markers: true,
+  },
+})
+interiorList.to('.sc-order-interior .group-interior .area1', {
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"
+}, 'a');
+interiorList.to('.sc-order-interior .group-interior .area2', {
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+}, 'a');
+interiorList.to('.sc-order-interior .group-interior .area2', {
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"
+}, 'b');
+interiorList.to('.sc-order-interior .group-interior .area3', {
+  "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+}, 'b');
 
 const textBg = gsap
   .timeline({
@@ -274,8 +279,8 @@ const textBg = gsap
 $(".sc-visual, #footer").mousemove(function (e) {
   const x = (20 * e.clientX) / $(".sc-visual").width();
   const y = (10 * e.clientY) / $(".sc-visual").height();
-
-  $(".sc-visual .img-box").css("transform", `translate(${x}px, -${y}px)`);
+  
+  gsap.to(".sc-visaul .img-box",{x:x,y:y})
 });
 
 $(".menu-btn").click(function () {
